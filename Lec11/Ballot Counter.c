15a99dc233993e9f065116ecc269b276b1d4433d
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-12-01 19:17:00 +0800

    Finish

---
+++ Lec11/Ballot Counter.c
@@ -0,0 +1,56 @@
+#include <stdio.h>                                  
+#include <string.h>                                                                                                                                                                          
+
+#define MAX_CAND_CNT 20
+#define MAX_NAME_LEN 32
+struct ballot_counter_s {
+    int count;
+    char name[MAX_NAME_LEN];
+} counter[MAX_CAND_CNT];
+int cand_cnt;
+
+void count(const char *name);
+const char *winner();
+
+int main()
+{
+    scanf("%d", &cand_cnt);
+    for (int i = 0; i < cand_cnt; i++) {
+        scanf("%s", counter[i].name);
+        counter[i].count = 0;
+    }
+
+    char name[MAX_NAME_LEN];
+    while (scanf("%s", name) != EOF)
+        count(name);
+
+    for (int i = 0; i < cand_cnt; i++)
+        printf("%s %d\n", counter[i].name, counter[i].count);
+
+    printf("%s\n", winner());
+
+    return 0;
+}
+
+
+void count(const char *name)
+{
+    for(int i=0;i<cand_cnt;i++){
+        if( strcmp(name,counter[i].name )==0 ){
+            counter[i].count++;
+        }
+    }
+}
+
+const char *winner()
+{
+    int mx=0,idx=0;
+    for(int i=0;i<cand_cnt;i++){
+        if( counter[i].count > mx ){
+            mx = counter[i].count;
+            idx=i;
+        }
+    }
+
+    return counter[idx].name;
+}
